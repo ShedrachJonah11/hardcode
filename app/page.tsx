1d94feb92fcbe8b9ce81be3222a946e4a23b8495
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-03-20 13:43:24 +0100

    feat: navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
-"use client"
+"use client";
 import * as React from "react";
 import Image from "next/image";
 import { NextUIProvider } from "@nextui-org/react";
+import NavBar from "@/components/NavBar";
+import bg2 from "../public/02.svg";
+import bg1 from "../public/01.svg";
 
 export default function Home() {
   return (
     <NextUIProvider>
-      <div>hello</div>
+      <main>
+        <NavBar />
+
+        <section>
+          <div className="relative bg-[#0E0C15] flex flex-col h-screen  justify-center items-center">
+            {/* Background image */}
+            <div className="absolute inset-0 z-0 mb-52">
+              <Image src={bg2} alt="bg" className="w-full h-full" />
+            </div>
+          </div>
+        </section>
+      </main>
     </NextUIProvider>
   );
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,28 @@
+"use client";
 import React from "react";
+import Image from "next/image";
+import logo from "../public/logo.svg";
+import Link from "next/link";
+import arrowright from "../public/arrow-right-01.svg";
+import { Button } from "@nextui-org/react";
 
-function NavBar() {
-  return <div></div>;
+export default function NavBar() {
+  return (
+    <div className="absolute bg-[#0C0B13] px-4 md:px-96 py-4 w-full z-50 bg-none">
+      <div className="flex border bg-[#0F0C17] border-slate-900 rounded-xl items-center justify-between px-3 py-1.5 ">
+        <Link href="/">
+          <h1>
+            <Image src={logo} alt="logo" width={125} height={30} />
+          </h1>
+        </Link>
+
+        <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-xl group bg-gradient-to-br to-[#8D1EA2] from-[#090EDB] group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white">
+          <span className="flex gap-2 relative px-12 py-2 transition-all ease-in text-sm duration-75 bg-white dark:bg-gray-900 rounded-xl group-hover:bg-opacity-0">
+            Connect
+            <Image src={arrowright} alt="" />
+          </span>
+        </button>
+      </div>
+    </div>
+  );
 }
-
-export default NavBar;
